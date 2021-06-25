25f27f8fb85dc99a7082cdb62dd2061928a6c4f8
Edgardo Medina <52425444+EMedina24@users.noreply.github.com>
2021-06-25 18:24:07 -0400

    Add files via upload

---
+++ gatsby-starter-default-master/src/components/Footer.js
@@ -0,0 +1,30 @@
+
+import * as React from "react"
+import PropTypes from "prop-types"
+import "./layout.css"
+
+const Footer = () => {
+ 
+  
+
+  return (
+    <>
+
+
+
+      <div>This is the footer</div>
+
+
+
+
+
+
+    </>
+  )
+}
+
+Footer.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Footer
